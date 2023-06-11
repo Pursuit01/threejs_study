234ece2bb26be483d106297a890d2f8d981ebbee
sunhongri <2724761434@qq.com>
2023-06-11 14:42:41 +0800

    feat: 物体缩放

--- src/main/main03-物体移动.js
+++ src/main/main03-物体移动.js
@@ -23,18 +23,17 @@ scene.add(camera);
 
 // 5. 添加物体
 // 创建几何体对象
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeGeometry = new THREE.BoxGeometry(2, 1, 1);
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: "skyblue" });
 
 // 根据几何体和材质创建物体
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
+// 设置几何体的位置
 // 定义坐标轴
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
-
-// 设置几何体的缩放比例
-cube.scale.set(3, 2, 1);
+// cube.position.set(5, 0, 0);
 
 // 将物体添加到场景中
 scene.add(cube);
@@ -56,6 +55,16 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const render = () => {
+  // 移动物体的x y z坐标
+  cube.position.x += 0.01;
+  cube.position.y += 0.01;
+  cube.position.z += 0.01;
+  // 到达阈值后重置
+  if (cube.position.x >= 5) {
+    cube.position.x = 0;
+    cube.position.y = 0;
+    cube.position.z = 0;
+  }
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 };
