f73fce54537f82de809945e2ad33600e9c1fee65
sunhongri <2724761434@qq.com>
2023-06-11 14:39:49 +0800

    feat: 物体移动

--- src/main/main.js
+++ src/main/main.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // console.log(THREE);
 
-// 目标： 使用控制器查看3d物体
+// 目标： 控制3d物体移动
 
 // 1. 创建一个场景
 const scene = new THREE.Scene();
@@ -28,6 +28,13 @@ const cubeMaterial = new THREE.MeshBasicMaterial({ color: "skyblue" });
 
 // 根据几何体和材质创建物体
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+// 设置几何体的位置
+// 定义坐标轴
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+// cube.position.set(5, 0, 0);
+
 // 将物体添加到场景中
 scene.add(cube);
 
@@ -48,6 +55,16 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const render = () => {
+  // 移动物体的x y z坐标
+  cube.position.x += 0.01;
+  cube.position.y += 0.01;
+  cube.position.z += 0.01;
+  // 到达阈值后重置
+  if (cube.position.x >= 5) {
+    cube.position.x = 0;
+    cube.position.y = 0;
+    cube.position.z = 0;
+  }
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 };
